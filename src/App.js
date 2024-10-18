4add410c4cd1ff8389576f6a69806dd3068af4ed
Matheus Bertemes <mathbertemes@gmail.com>
2024-10-17 18:28:56 -0700

    app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import * as Styled from './App.styles.jsx'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+  const products = [
+      {
+      name: 'Product 1',
+      description: 'Description of product 1',
+      price: '$10.99'
+      },
+      {
+      name: 'Product 2',
+      description: 'Description of product 2',
+      price: '$24.99'
+      },
+      {
+      name: 'Product 3',
+      description: 'Description of product 3',
+      price: '$15.49'
+      },
+    ];
+    
+    return (
+      <div>
+      {products.map((product, index) => (
+      <Styled.ProductContainer key={index}>
+      <Styled.ProductName>{product.name}</Styled.ProductName>
+      <div>{product.description}</div>
+      <Styled.ProductPrice>{product.price}</Styled.ProductPrice>
+      </Styled.ProductContainer>
+      ))}
+      </div>
+      );
 }
 
 export default App;
